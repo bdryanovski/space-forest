8f013279e13e301c89f55698f9a1880214e8ecd5
Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>
2019-12-21 14:06:54 +0200

    fix: update package.json and getting started
    Signed-off-by: Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>

--- src/screens/getting-started.js
+++ src/screens/getting-started.js
@@ -1,14 +1,31 @@
 import React from 'react'
-import PrismCode from './components/prism'
+import Code from './components/prism'
 
 export default class GettingStarted extends React.Component {
   render() {
     return (
       <>
         <h4>Getting started</h4>
-        <PrismCode language="bash">
+        <Code language="bash">
           {'npm install --save package-name'}
-        </PrismCode>
+        </Code>
+
+        <p>
+          Import styles inside React project
+        </p>
+        <Code language="javascript">
+          {`
+          import styles from '<pkg-name>/dist/<pkg-name>.css'
+            `}
+        </Code>
+        <p>
+          Import inside SCSS file.
+        </p>
+        <Code language="css">
+          {`
+          @import "node_modules/<pkg-name>/dist/<pkg-name>css";
+          `}
+        </Code>
       </>
     )
   }
