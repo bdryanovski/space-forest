52becda11fc3d3148d8fe1a7bff34080f280ce5e
Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>
2019-12-24 23:23:34 +0200

    build: adding SCSS styles
    Signed-off-by: Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>

---
+++ build/precache-manifest.efab4fd5cc976613fdeccf5efe8beb12.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67935e997589929a022c158d97c96fc5",
+    "url": "/editor.worker.js"
+  },
+  {
+    "revision": "df5f49670a5b8428d4b7785f0b0fd985",
+    "url": "/html.worker.js"
+  },
+  {
+    "revision": "17322ef124eb464160d2aca42dc0e154",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffc416ec3694fb91b875",
+    "url": "/static/css/2.77bad4cb.chunk.css"
+  },
+  {
+    "revision": "82a9451dc23eb4722c0e",
+    "url": "/static/css/main.2e26fa90.chunk.css"
+  },
+  {
+    "revision": "ffc416ec3694fb91b875",
+    "url": "/static/js/2.3d83dedf.chunk.js"
+  },
+  {
+    "revision": "d291710617b2b68fc3d4634558f6c3e3",
+    "url": "/static/js/2.3d83dedf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "302a7bf2ca8ed5dafe94",
+    "url": "/static/js/3.35f570ad.chunk.js"
+  },
+  {
+    "revision": "fec3317c1b95e3053462",
+    "url": "/static/js/4.e2280494.chunk.js"
+  },
+  {
+    "revision": "654a6631fd994366d8d5",
+    "url": "/static/js/5.d8eb066e.chunk.js"
+  },
+  {
+    "revision": "82a9451dc23eb4722c0e",
+    "url": "/static/js/main.95a779f5.chunk.js"
+  },
+  {
+    "revision": "622c9c4d62205ec9897f",
+    "url": "/static/js/runtime-main.5d46e6ed.js"
+  },
+  {
+    "revision": "3ac49cb33f43a6471f21ab3df40d1b1e",
+    "url": "/static/media/fa-regular-400.3ac49cb3.eot"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/static/media/fa-regular-400.9efb8697.woff2"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/static/media/fa-regular-400.a57bcf76.woff"
+  },
+  {
+    "revision": "d2e53334c22a9a4937bc26e84b36e1e0",
+    "url": "/static/media/fa-regular-400.d2e53334.svg"
+  },
+  {
+    "revision": "ece54318791c51b52dfdc689efdb6271",
+    "url": "/static/media/fa-regular-400.ece54318.ttf"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/static/media/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "7a5de9b08012e4da40504f2cf126a351",
+    "url": "/static/media/fa-solid-900.7a5de9b0.svg"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/static/media/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/static/media/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/static/media/fa-solid-900.f6121be5.woff2"
+  }
+]);
