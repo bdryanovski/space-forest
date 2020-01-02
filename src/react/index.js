eeac7561e035c8109b4e475ee4f9bc9b70d3c5d9
Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>
2020-01-02 15:35:27 +0200

    fix exports for react
    Signed-off-by: Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>

--- src/react/index.js
+++ src/react/index.js
@@ -4,3 +4,9 @@ export * from './article'
 export {
   Header1, Header2, Header3, Header4, Header5, Header6
 } from './header/index.js'
+export * from './footer'
+export * from './navigation'
+export * from './navigation-item'
+export * from './pagination'
+export * from './pagination-item'
+export * from './theme/theme-switcher'
