d5493bf8c4c59be8aeaf2208fde93e475668bd2e
Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>
2020-01-15 14:56:29 +0200

    build: adding SCSS styles
    Signed-off-by: Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d76861248a13fd0ecafb48f027a47302.js"
+  "/precache-manifest.8a3c414084df12ee2db35d8a1167f2a5.js"
 );
 
 self.addEventListener('message', (event) => {
