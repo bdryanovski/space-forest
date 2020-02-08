df185e5376086bf447072db5c36f7072aaa90844
Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>
2020-02-08 22:03:23 +0200

    docs: update landing page and adding some animation to it
    Signed-off-by: Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,17 +1,103 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
+import './styles/home.scss'
+
 export default class Home extends React.Component {
   render() {
     return (
       <>
         <div className="container">
-          <div className="banner">
-            <h2>UI Framework</h2>
-            <p>Static Styles & React Wrappers</p>
+
+          <div className="bg">
+            <div className="sky">
+              <div className="forest">
+                <div className="forest__item">
+                  <div className="forest__tree forest__tree--1"></div>
+                  <div className="forest__tree forest__tree--2"></div>
+                  <div className="forest__tree forest__tree--3"></div>
+                  <div className="forest__tree forest__tree--4"></div>
+                </div>
+                <div className="forest__item">
+                  <div className="forest__tree forest__tree--1"></div>
+                  <div className="forest__tree forest__tree--2"></div>
+                  <div className="forest__tree forest__tree--3"></div>
+                  <div className="forest__tree forest__tree--4"></div>
+                </div>
+                <div className="forest__item">
+                  <div className="forest__tree forest__tree--1"></div>
+                  <div className="forest__tree forest__tree--2"></div>
+                  <div className="forest__tree forest__tree--3"></div>
+                  <div className="forest__tree forest__tree--4"></div>
+                </div>
+                <div className="forest__item">
+                  <div className="forest__tree forest__tree--1"></div>
+                  <div className="forest__tree forest__tree--2"></div>
+                  <div className="forest__tree forest__tree--3"></div>
+                  <div className="forest__tree forest__tree--4"></div>
+                </div>
+                <div className="forest__item">
+                  <div className="forest__tree forest__tree--1"></div>
+                  <div className="forest__tree forest__tree--2"></div>
+                  <div className="forest__tree forest__tree--3"></div>
+                  <div className="forest__tree forest__tree--4"></div>
+                </div>
+                <div className="forest__item">
+                  <div className="forest__tree forest__tree--1"></div>
+                  <div className="forest__tree forest__tree--2"></div>
+                  <div className="forest__tree forest__tree--3"></div>
+                  <div className="forest__tree forest__tree--4"></div>
+                </div>
+              </div>
+              <div className="main">
+                <div className="tent">
+                  <div className="tent__side"></div>
+                  <div className="tent__front">
+                    <div className="tent__door"></div>
+                  </div>
+                </div>
+                <div className="tree">
+                  <div className="tree__item tree__item--1"></div>
+                  <div className="tree__item tree__item--2"></div>
+                </div>
+                <div className="tree">
+                  <div className="tree__item tree__item--3"></div>
+                  <div className="tree__item tree__item--4"></div>
+                </div>
+                <div className="rock">
+                  <div className="rock__item rock__item--1"></div>
+                  <div className="rock__item rock__item--2"></div>
+                  <div className="rock__item rock__item--3"></div>
+                </div>
+                <div className="cloud cloud--1">
+                  <div className="cloud__item"></div>
+                </div>
+                <div className="cloud cloud--2">
+                  <div className="cloud__item"></div>
+                </div>
+                <div className="cloud cloud--3">
+                  <div className="cloud__item"></div>
+                </div>
+
+                <div className="fire">
+                  <div className="fire__item"></div>
+                  <div className="fire__trunk fire__trunk--1"></div>
+                  <div className="fire__trunk fire__trunk--2"></div>
+                  <div className="fire__light"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          <div className="onTOP">
+            <h1>SpaceForest Framework</h1>
+            <p>Static HTML & CSS styles and React components.</p>
             <Link to="/documentation" className="btn primary">Getting Started</Link>
           </div>
+
         </div>
+
       </>
     )
   }
