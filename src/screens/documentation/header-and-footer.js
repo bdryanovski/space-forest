428409df5f76638a92162adba3fe19b020b94469
Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>
2020-02-09 12:38:07 +0200

    docs: update navigation examples and styles
    Signed-off-by: Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>

--- src/screens/documentation/header-and-footer.js
+++ src/screens/documentation/header-and-footer.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {Navigation, NavigationItem, Footer} from '../../react-components'
 import DemoPlayground from '../../react-components/playground/demo-playground'
+import Code from '../components/code'
 
 const NAV_SCOPE = {Navigation, NavigationItem}
 const FOOTER_SCOPE = {Footer}
@@ -10,9 +11,18 @@ const NAV_HORIZONTAL = `
   <NavigationItem>Logo</NavigationItem>
   <NavigationItem><a>Home</a></NavigationItem>
   <NavigationItem><a>Posts</a></NavigationItem>
-  <NavigationItem className="active"><a>Pages</a></NavigationItem>
+  <NavigationItem><a className="active">Pages</a></NavigationItem>
   <NavigationItem><a>Categories</a></NavigationItem>
   <NavigationItem className="right"><a>Settings</a></NavigationItem>
+</Navigation>
+`
+
+const NAV_VERTICAL = `
+<Navigation type="vertical">
+  <NavigationItem><a>Home</a></NavigationItem>
+  <NavigationItem><a>Posts</a></NavigationItem>
+  <NavigationItem><a className="active">Pages</a></NavigationItem>
+  <NavigationItem><a>Categories</a></NavigationItem>
 </Navigation>
 `
 const FOOTER = '<Footer>Copyright { (new Date().getFullYear()) }</Footer>'
@@ -28,6 +38,28 @@ export default class HeaderFooter extends Component {
         <p>Simple box that will create navigation header, depends on Grid2</p>
         <DemoPlayground code={NAV_HORIZONTAL} scope={NAV_SCOPE} language="html"/>
 
+        <p>There is also vertical navigation <code>type</code></p>
+        <DemoPlayground code={NAV_VERTICAL} scope={NAV_SCOPE} language="html" />
+
+        <p>There is as well a static version</p>
+        <Code language="html">
+          {
+            `
+<nav>
+  <ul class="horizontal">
+    <!-- Every 'li' is a new entry -->
+    <li><a href="#">Home</a></li>
+    <li><a href="#" class="active">Documentation</a></li>
+  </ul>
+</nav>
+            `
+          }
+        </Code>
+
+        <p>
+          There two main clasess <code>.horizontal</code> and <code>.vertical</code>.
+          There is also a class to mark a entry as active, <code>.active</code>
+        </p>
 
         <h2>Footer</h2>
         <DemoPlayground code={FOOTER} scope={FOOTER_SCOPE} language="html"/>
