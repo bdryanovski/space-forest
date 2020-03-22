ed256ff8ef03881626c840842367c9e4ba4bfa04
Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>
2020-03-22 19:31:09 +0200

    breaking: Droping Grid and Layout documentation page frm CRA
    Signed-off-by: Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>

--- src/router.js
+++ src/router.js
@@ -10,7 +10,6 @@ import ThemeSwitcher from './react-components/theme/theme-switcher'
 import Home from './screens/home.js'
 import GettingStarted from './screens/getting-started'
 import Documentation from './screens/documentation'
-import GridDemo from './screens/documentation/grid'
 import LinksButtonsDemo from './screens/documentation/links-buttons'
 import IconsDemo from './screens/documentation/icons'
 import CardsDemo from './screens/documentation/cards'
@@ -20,7 +19,6 @@ import Pagination from './screens/documentation/pagination'
 import ThemeDemo from './screens/documentation/theme'
 import ModalDemo from './screens/documentation/modal-demo'
 import PlaceholderDemo from './screens/documentation/placeholder-demo'
-import LayoutDemo from './screens/documentation/layouts/layouts-demo'
 import EditorDemo from './screens/documentation/editor'
 import FormsDocumentation from './screens/documentation/forms'
 import TableDocs from './screens/documentation/table-docs'
@@ -45,8 +43,6 @@ export const routes = [
       {name: 'Changelog', path: '/documentation/changelog', component: ChangelogDocs },
       {name: 'Theme', path: '/documentation/theme', component: ThemeDemo},
       {name: 'UI', path: '/documentation/ui', component: UIDemo },
-      {name: 'Grid', path: '/documentation/grid', component: GridDemo},
-      {name: 'Layout', path: '/documentation/layout', component: LayoutDemo},
       { name: 'Forms', path: '/documentation/forms', component: FormsDocumentation },
       {
         name: 'Links & Buttons',
