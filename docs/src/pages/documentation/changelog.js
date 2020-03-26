19e02bfef1556786948621339e8b9316fd3acb4c
Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>
2020-03-26 15:38:59 +0200

    build: start working on changelog page
    Signed-off-by: Bozhidar Dryanovski <bozhidar.dryanovski@gmail.com>

--- docs/src/pages/documentation/changelog.js
+++ docs/src/pages/documentation/changelog.js
@@ -2,22 +2,49 @@ import React from 'react'
 import Layout from '@theme/Layout'
 import PKG from '../../package'
 
-//import CLOG from '../../../../CHANGELOG.md'
+import ChangeLogFile from '../../../changelog.json'
+
+console.log(ChangeLogFile)
 
 function Changelog() {
+
+  function renderLog(l) {
+    return (
+      <>
+        <h2>{l.version} ({l.date})</h2>
+        {
+          Object.keys(l.changes).map((topic, i) => {
+            return (
+              <ul key={i}>
+                <li id={topic} >{l.changes[topic].description}</li>
+                <ul>
+                  {
+                    l.changes[topic].changes.map((change, z) => {
+                      return (
+                        <li key={z}>
+
+                          { change.category ? `(${change.category})` : '' }
+                          {change.subject}
+                          {' '}
+                          [<a href={change.githubLink}>{ change.hash}</a>]
+                        </li>
+                      )
+                    })
+                  }
+                </ul>
+              </ul>
+            )
+          })
+        }
+      </>
+
+    )
+  }
+
   return (
     <Layout title="Hello">
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '50vh',
-          fontSize: '20px',
-        }}>
-        <p>
-          Extract the changelog from the project CHANGELOG.md
-        </p>
+      <div>
+        {renderLog(ChangeLogFile) }
       </div>
     </Layout>
   )
